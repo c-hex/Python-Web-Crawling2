766eadee1020ab3e7daa0b223af4aa010bd8e8f2
류채연 <haha03270@gmail.com>
2023-05-08 15:45:24 +0900

    Create oliveyoung.py

---
+++ oliveyoung.py
@@ -0,0 +1,19 @@
+import bs4
+import requests
+from bs4 import BeautifulSoup
+
+def main():
+    url = "https://www.oliveyoung.co.kr/store/main/getHotdealPagingListAjax.do?date=20230508&pageIdx=1&fltCondition=02&fltDispCatNo=&prdSort=rank"
+    data = requests.get(url)
+    if data.status_code == 200:
+        soup = bs4.BeautifulSoup(data.text, "html.parser")
+        prodList = soup.find_all("li")
+        for product in prodList:
+            print(product.find("img")["src"])
+            print("할인 : " + product.select_one(".Pnum").text)
+            print(product.select_one(".prod-name").text)
+            print(product.select_one(".total").text)
+            print("-------")
+
+if __name__ == '__main__':
+    main()
